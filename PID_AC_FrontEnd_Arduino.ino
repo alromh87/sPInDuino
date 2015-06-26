3d81d2a332c055763242270e9c3226cf6b575440
Agustin <magistr2@gmail.com>
2015-06-26 01:14:04 -0500

    Calculo de RPMs por define TODO: validar calculo
    Atención: No jala!!!

--- PID_AC_FrontEnd_Arduino.ino
+++ PID_AC_FrontEnd_Arduino.ino
@@ -9,6 +9,7 @@
 #include <PID_v1.h>
 
 //#define DEBUG 1
+#define FAST_ALG 1
 
 /* Definicion de pines */
 #define INPUT_PIN                 0                //Referncia Analogica de valor deseado
@@ -25,18 +26,15 @@
 volatile byte zeroBit = LOW; // declare IRQ flag
  // HIGH = 1, LOW = 0
   
-unsigned long rpmcount;
+volatile unsigned long rpmcount;
+volatile unsigned long time;
+volatile unsigned long timeold;
+
 unsigned long rpm;
-unsigned long time;
-unsigned long timeold;
 float impuls_time;
 
-unsigned long timeMark = 0;
-int rpm2=0;
-  
-//unsigned int retraso;
-float retraso;
-float maxDelay = .6*(1000000/(float)120); //Retraso maximo: Perido de media señal senoidal en Microsegundos escalado al 90%
+unsigned int retraso;
+float maxDelay = .7*(1000000/(float)120); //Retraso maximo: Perido de media señal senoidal en Microsegundos escalado al 90%
 
 
 //Define Variables we'll be connecting to
@@ -51,9 +49,9 @@ double Setpoint, Input, Output;
     //double consKp=0.1, consKi=0.5, consKd=0.05;
 
 //Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint,consKp, consKi, consKd, REVERSE);
+PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, REVERSE);
 
-unsigned long serialTime; //this will help us know when to talk with processing
+unsigned long serialTime=0; //this will help us know when to talk with processing
 
 void setup()
 {
@@ -62,11 +60,10 @@ void setup()
 //  Serial.begin(9600);
   
   //initialize the variables we're linked to
-  Input = analogRead(INPUT_PIN);
-  Setpoint = 100;
-
-  //turn the PID on
-  myPID.SetMode(AUTOMATIC);
+  Input = 0;
+  
+  pinMode(INPUT_PIN, INPUT);
+  Setpoint = analogRead(INPUT_PIN);
 
 //Triac control setup  
   pinMode(TRIAC_CONTROL, OUTPUT);  
@@ -81,7 +78,9 @@ void setup()
   digitalWrite(RPM_SENSOR, 1); // pull up on
   attachInterrupt(1, rpm_fun, FALLING);  // interrupt 1 digital pin 3 connected hall sensor
 
-  Serial.print("Iniciado ");
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+
 }
 
 void loop(){
@@ -103,42 +102,48 @@ if(Setpoint<255)return;
 //TRIAC delay control      
   if (zeroBit == 1){
     
-    //PID   
-    time = micros() - timeold;
-    float time_in_sec  = (float)time / 1000000;
-    impuls_time = (float)rpmcount / time_in_sec;
+    //PID
+#ifdef FAST_ALG
+    rpm = (60*(1000000/(time-timeold))) / MARCAS_SENSOR;
+#else
+    time = micros();
+    
+    impuls_time = 1000000*(float)rpmcount / (float)(time-timeold);
+    
     rpm                = impuls_time*60 / MARCAS_SENSOR;
-
     timeold  = micros(); //set time
-    rpmcount = 0; //reset
-  
+#endif
+
     Input = (rpm/(float)RPM_MAX)*REF_MAX;
   
     myPID.Compute();
 
-//    retraso = (Output*(float)maxDelay)/255;
-    retraso = (Output*(float)maxDelay)/255.0;
+    retraso = (Output*(float)maxDelay)/255;
+//if(Output>1023)Output=1023;
+//    retraso = (unsigned int)(Output*(float)maxDelay)/255.0;
+//    retraso=0;
     
     if(retraso>0)
-      delayMicroseconds((int)retraso);
+      delayMicroseconds(retraso);
     digitalWrite(TRIAC_CONTROL, 1); //triac on 
     delayMicroseconds(100); 
     digitalWrite(TRIAC_CONTROL, 0);  //triac off 
     zeroBit = 0; // clear flag
 
+
     #ifdef DEBUG
       Serial.print(rpmcount);
       Serial.print(" marcas en ");
-      Serial.print(time);
+      Serial.print(time-timeold);
       Serial.print("us ( ");
       Serial.print(impuls_time);
       Serial.print(") RPM: ");
       Serial.print(rpm);
-      Serial.print(" vs  ");
-      Serial.print(rpm2);
       Serial.print("   Retraso: ");
       Serial.println(retraso);
     #endif
+
+    rpmcount = 0; //reset
   }
 }
 
@@ -253,9 +258,11 @@ void zero_fun(){
   } 
 
 void rpm_fun(){
-   unsigned long timeT = micros();
-   rpm2 =(1000000/(8.0*(timeT-timeMark)))*60;
+  //Each rotation, this interrupt function is run
+#ifdef FAST_ALG
+   timeold = time;
+   time = micros();
+#else
    rpmcount++;
-   timeMark = timeT;
-   //Each rotation, this interrupt function is run
+#endif
  }
